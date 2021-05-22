ffebf5e1b7ebd5d4fcf6054dbc1fa52bce407815
Konstantin Läufer <klaeufer@gmail.com>
2021-05-21 19:31:42 -0500

    updated .gitignore
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/IntervalTree.scala
+++ src/main/scala/IntervalTree.scala
@@ -5,8 +5,8 @@ class IntervalTree {
   case class Interval(start: Int, end: Int) extends Ordered[Interval] {
     def compare(that: Interval) = (start compare that.start, end compare that.end) match {
       case (-1, -1) => -1
-      case (1, 1) => 1
-      case _ => 0
+      case (1, 1)   => 1
+      case _        => 0
     }
     def contains(point: Int) = start <= point && point <= end
     def intersects(that: Interval) = (that contains start) || (that contains end)
@@ -19,7 +19,7 @@ class IntervalTree {
   }
 
   import Interval._ // Add "Interval" methods to Range
-  
+
   case class Node(byStart: List[Range], byEnd: List[Range]) {
     def this(r: Range) = this(List(r), List(r))
     def this(point: Int) = this(point to point)
@@ -38,22 +38,22 @@ class IntervalTree {
     def apply(r: Range) = new Node(r)
     def apply(point: Int) = new Node(point)
   }
-  
+
   object RedBlackInterval extends scala.collection.immutable.RedBlack[Range] {
     def isSmaller(x: Range, y: Range) = x < y
   }
   import RedBlackInterval._
-  
+
   var root: Tree[Node] = Empty
-  
+
   def insert(r: Range) = {
-      root lookup r match {
-      case Empty => root = root update (r.centralized, Node(r))
+    root lookup r match {
+      case Empty           => root = root update (r.centralized, Node(r))
       case ne: NonEmpty[_] => root = root update (ne.key, ne.value insert r)
     }
     this
   }
-  
+
   def delete(r: Range) = {
     root lookup r match {
       case Empty => ()
@@ -66,49 +66,49 @@ class IntervalTree {
     }
     this
   }
-  
+
   def lookup(r: Range) = {
     def search(t: Tree[Node]): (List[Range], List[Range]) = t match {
       case Empty => (Nil, Nil)
       case ne: NonEmpty[_] =>
         val (byStart, byEnd) = if (r < ne.key) search(ne.left) else search(ne.right)
         ((ne.value.byStart filter (r intersects _)) ++ byStart,
-         (ne.value.byEnd filter (r intersects _)) ++ byEnd)
+          (ne.value.byEnd filter (r intersects _)) ++ byEnd)
     }
     search(root)
   }
   def lookup(point: Int): (List[Range], List[Range]) = lookup(point to point)
-  
+
   def range(from: Option[Range], to: Option[Range]) =
     root.range(from, to).iterator.foldLeft((Nil, Nil): (List[Range], List[Range])) {
       case ((bS, bE), (_, n)) => (bS ::: n.byStart, bE ::: n.byEnd)
     }
-    
+
   def iterator = root.elements map (n => (n._2.byStart, n._2.byEnd))
   def iteratorByStart = root.elements flatMap (_._2.byStart.elements)
   def iteratorByEnd = root.elements flatMap (_._2.byEnd.elements)
 
-  def rangeToString(r: Range) = "[%d%s]" format (r.first, if (r.last == r.first) "" else ","+r.last)
+  def rangeToString(r: Range) = "[%d%s]" format (r.first, if (r.last == r.first) "" else "," + r.last)
 
   def treeToString(t: Tree[Node], level: Int): String = t match {
     case Empty => "E\n"
     case BlackTree(key, value, left, right) =>
       "Black Center: %s\tRanges: %s\n%s|\n%s+L: %s%s|\n%s+R: %s" format
         (key, value.byStart map rangeToString mkString ",",
-         "|"*level,
-         "|"*level, treeToString(left, level + 1),
-         "|"*level,
-         "|"*level, treeToString(right, level + 1))
+          "|" * level,
+          "|" * level, treeToString(left, level + 1),
+          "|" * level,
+          "|" * level, treeToString(right, level + 1))
     case RedTree(key, value, left, right) =>
       "Red Center: %s\tRanges: %s\n%s|\n%s+L: %s%s|\n%s+R: %s" format
         (key, value.byStart map rangeToString mkString ",",
-         "|"*level,
-         "|"*level, treeToString(left, level + 1),
-         "|"*level,
-         "|"*level, treeToString(right, level + 1))
-    case other => error("Unexpected "+other)
+          "|" * level,
+          "|" * level, treeToString(left, level + 1),
+          "|" * level,
+          "|" * level, treeToString(right, level + 1))
+    case other => error("Unexpected " + other)
   }
 
-  override def toString = "\n"+treeToString(root, 0)
+  override def toString = "\n" + treeToString(root, 0)
 }
 
--- src/test/scala/IntervalTreeCheck.scala
+++ src/test/scala/IntervalTreeCheck.scala
@@ -3,22 +3,23 @@ import Prop._
 import Arbitrary.arbitrary
 
 object IntervalTreeCheck extends Properties("IntervalTree") {
-  val smallInteger = Gen.choose(0,500) // Avoid stack overflows causes by toString on very big ranges
+  val smallInteger = Gen.choose(0, 500) // Avoid stack overflows causes by toString on very big ranges
   val myRangeGen = for {
     start <- smallInteger
     end <- smallInteger
   } yield if (start < end) start to end else end to start
   implicit def arbRange: Arbitrary[Range] = Arbitrary(myRangeGen)
-  
+
   val myIntervalTreeGen = for {
     l <- Gen.listOf(myRangeGen)
   } yield l.foldLeft(new IntervalTree){ (t, r) => t insert r; t }
   implicit def arbIntervalTree: Arbitrary[IntervalTree] = Arbitrary(myIntervalTreeGen)
-  
+
   property("All intervals containing a point are returned") =
-    forAll { (t: IntervalTree, n: Int) => n < 1000 ==>
-      (t.iteratorByStart.toList.filter(_ contains n) == t.lookup(n)._1) :|
-        "Expected: "+(t.iteratorByStart.toList.filter(_ contains n).map(t.rangeToString)+
-        "\nGot: "+t.lookup(n)._1.map(t.rangeToString))
+    forAll { (t: IntervalTree, n: Int) =>
+      n < 1000 ==>
+        (t.iteratorByStart.toList.filter(_ contains n) == t.lookup(n)._1) :|
+        "Expected: " + (t.iteratorByStart.toList.filter(_ contains n).map(t.rangeToString) +
+          "\nGot: " + t.lookup(n)._1.map(t.rangeToString))
     }
 }
